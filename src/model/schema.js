d19a14dd2f4882751a95cc4b21236bc6b208e90e
Julien Vincent <julienlucvincent@gmail.com>
2016-02-29 22:13:50 +0200

    swapped out CheapSchema with a MODELIZR_CHEAP_MOCK env

--- src/model/schema.js
+++ src/model/schema.js
@@ -1,64 +1,59 @@
 import { Schema as NormalizerSchema, arrayOf } from 'normalizr'
 import _ from 'lodash'
 
-const makeSchema = isCheap => {
-    let jsf = () => ({})
+let jsf = () => ({})
+if (!process.env.MODELIZR_CHEAP_MOCK) {
+    jsf = require('json-schema-faker')
+}
 
-    if (!isCheap) {
-        jsf = require('json-schema-faker')
+class Schema extends NormalizerSchema {
+
+    constructor(schema, options) {
+        if (!schema.key) {
+            throw new Error('Schema object must have a key property')
+        }
+        if (typeof schema.key !== 'string') {
+            throw new Error('Schema key must be a string')
+        }
+
+        super(schema.key, options)
+
+        this._define = super.define
+        this._schema = {
+            ...schema,
+            ...{
+                type: 'object'
+            }
+        }
     }
 
-    return class Schema extends NormalizerSchema {
+    getProperties() {
+        return this._schema.properties
+    }
 
-        constructor(schema, options) {
-            if (!schema.key) {
-                throw new Error('Schema object must have a key property')
+    define(schemas) {
+        this._define(_.mapValues(this._schema.definitions, definition => {
+            if (Array.isArray(definition)) {
+                return arrayOf(schemas[definition])
             }
-            if (typeof schema.key !== 'string') {
-                throw new Error('Schema key must be a string')
+            if (typeof definition === 'string') {
+                return schemas[definition]
             }
+            return definition
+        }))
+    }
 
-            super(schema.key, options)
-
-            this._define = super.define
-            this._schema = {
-                ...schema,
-                ...{
-                    type: 'object'
-                }
+    mock(ids) {
+        if (ids) {
+            if (typeof ids === 'number') {
+                return _.set(jsf(this._schema), 'id', ids)
+            } else if (Array.isArray(ids)) {
+                return _.map(ids, id => _.set(jsf(this._schema), 'id', id))
             }
-        }
-
-        getProperties() {
-            return this._schema.properties
-        }
-
-        define(schemas) {
-            this._define(_.mapValues(this._schema.definitions, definition => {
-                if (Array.isArray(definition)) {
-                    return arrayOf(schemas[definition])
-                }
-                if (typeof definition === 'string') {
-                    return schemas[definition]
-                }
-                return definition
-            }))
-        }
-
-        mock(ids) {
-            if (ids) {
-                if (typeof ids === 'number') {
-                    return _.set(jsf(this._schema), 'id', ids)
-                } else if (Array.isArray(ids)) {
-                    return _.map(ids, id => _.set(jsf(this._schema), 'id', id))
-                }
-            } else {
-                return jsf(this._schema)
-            }
+        } else {
+            return jsf(this._schema)
         }
     }
 }
 
-const Schema = makeSchema(false)
-
-export { Schema as default, Schema, makeSchema as CheapSchema }
+export { Schema as default, Schema }
